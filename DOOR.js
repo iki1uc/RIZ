beebf8ea79d1b257caa453b04a5de83bffd07e88
iki1uc <entertrainer@web.de>
2026-08-17 02:39:41 +0200

    Update DOOR.js

--- DOOR.js
+++ DOOR.js
@@ -1,1 +1,13 @@
+export const DOOR = {
+  name: "DOOR",
+  role: "Kooridor-Öffnung",
+  ability: {
+    station: "öffnen",
+    orbit: "durchlassen",
+    kooridor: "aktivieren"
+  },
+  layer: "octa²",
+  type: "RIZ-front",
+  total: 1
+};
 
