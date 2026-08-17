d92b341d0128d8b997a0363477a92e0961e70431
iki1uc <entertrainer@web.de>
2026-08-17 02:39:54 +0200

    Update DOO.js

--- DOO.js
+++ DOO.js
@@ -1,1 +1,12 @@
-
+export const DOO = {
+  name: "DOO",
+  role: "Kooridor-Abfluss",
+  ability: {
+    station: "abführen",
+    orbit: "stabilisieren",
+    kooridor: "schließen"
+  },
+  layer: "octa²",
+  type: "RIZ-back",
+  total: 1
+};
