7394ee873735b514c234cf539daa4166daf581e8
iki1uc <entertrainer@web.de>
2026-08-17 02:26:12 +0200

    Update YxY.js

--- YxY.js
+++ YxY.js
@@ -1,1 +1,12 @@
+export const YxY = {
+  ausweg3E: [
+    "DOOR",   // Öffnet den Ausweg
+    "DOO",    // Leitet Energie ab
+    "SHELL"   // Schützt den Abfluss
+  ],
+
+  layer: "octa²",
+  type: "RIZ-ausweg",
+  total: 3
+};
 
